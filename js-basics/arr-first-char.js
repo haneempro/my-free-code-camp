4c2d0b99375e60d582f997cb7c8108ebb9723199
Meena <86232878+haneempro@users.noreply.github.com>
2021-07-21 10:22:13 +0100

    Access first character of an element in an array

---
+++ js-basics/arr-first-char.js
@@ -0,0 +1,6 @@
+// Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0]; 
