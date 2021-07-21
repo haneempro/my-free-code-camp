ec2e9ba73ec8add9c63b6dc3415a1d93bf871663
Meena <86232878+haneempro@users.noreply.github.com>
2021-07-21 10:23:53 +0100

    Access last character

---
+++ js-basics/arr-last-char.js
@@ -0,0 +1,7 @@
+// Use bracket notation to find the last character in the lastName variable.
+
+var lastName = "Lovelace";
+
+var lastLetterOfLastName = lastName[lastName.length -1]
+
+console.log(lastLetterOfLastName);
