816796fd6e876dede3af211c934a5b250d01ba9c
Meena <86232878+haneempro@users.noreply.github.com>
2021-07-21 10:20:24 +0100

    Access elements in a multi dimensional array

---
+++ js-basics/arr-mullti-di.js
@@ -0,0 +1,6 @@
+// The first set of brackets refers to the entries in the outer-most (the first level) array, and each additional pair of brackets refers to the next level of entries inside */
+
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[0][0]; // bracket notation select an element from myArray so that myData is equal to 8.
