fd9a68e44b72b23a806350ac64ff3c7443dc08d5
Meena <86232878+haneempro@users.noreply.github.com>
2021-07-21 10:25:09 +0100

    Access nth to last char

---
+++ js-basics/arr-nth-last-char.js
@@ -0,0 +1,7 @@
+//Use bracket notation to find the second-to-last character in the lastName string.
+
+var lastName = "Lovelace";
+
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+console.log(secondToLastLetterOfLastName)
